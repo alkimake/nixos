b416cd2b9cc0a28f210da374567ff47840efbef9
Alkim Ake Gozen <alkimake@pm.me>
2024-06-06 14:07:11 +0900

    nixos-hardware for x1 gen7 laptop

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
--- hosts/X1/configuration.nix
+++ hosts/X1/configuration.nix
@@ -7,6 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
+      inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.default
     ];
@@ -56,10 +57,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-     neovim
-     git
+    sof-firmware
   ];
   
   programs.zsh.enable = true;
