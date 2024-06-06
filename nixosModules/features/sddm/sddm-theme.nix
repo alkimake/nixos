866330d578bd179a7587b3c653979052befd22dc
Alkim Ake Gozen <alkimake@pm.me>
2024-06-06 18:45:12 +0900

    sddm-theme: tokyonight theme

--- nixosModules/features/sddm/sddm-theme.nix
+++ nixosModules/features/sddm/sddm-theme.nix
@@ -1,24 +1,24 @@
 {pkgs}: let
   image = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/Goxore/dotfiles/50db864d56d49768f1d4d0a8c1bd7a5c74dd629e/home/Wallpapers/gruvbox-mountain-village.png";
-    sha256 = "sha256-HrcYriKliK2QN02/2vFK/osFjTT1NamhGKik3tozGU0=";
+    url = "https://raw.githubusercontent.com/siddrs/tokyo-night-sddm/main/Backgrounds/shacks.png";
+    sha256 = "sha256-lWwBcbczNkThAHPze1juJJ1LAI8lUoMNqbm99rD+K0k=";
   };
 in
   pkgs.stdenv.mkDerivation {
     name = "sddm-theme";
 
     src = pkgs.fetchFromGitHub {
-      owner = "MarianArlt";
-      repo = "sddm-sugar-dark";
-      rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
-      sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
+      owner = "siddrs";
+      repo = "tokyo-night-sddm";
+      rev = "320c8e74ade1e94f640708eee0b9a75a395697c6";
+      sha256 = "JRVVzyefqR2L3UrEK2iWyhUKfPMUNUnfRZmwdz05wL0=";
     };
     installPhase = ''
       mkdir -p $out
       cp -R ./* $out/
       cd $out/
-      rm Background.jpg
-      cp -r ${image} $out/Background.jpg
+      rm Backgrounds/win11.png
+      cp -r ${image} $out/Backgrounds/win11.png
     '';
   }
 
