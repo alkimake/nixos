54aefd0e6123f9c1b6273e50160d2312e29fa0a6
Alkim Ake Gozen <alkimake@pm.me>
2024-06-09 18:22:49 +0900

    sddm: test catpuccin mocha

--- nixosModules/features/sddm/default.nix
+++ nixosModules/features/sddm/default.nix
@@ -1,15 +1,19 @@
-{pkgs, lib, ...}: let
-  sddmTheme = import ./sddm-theme.nix {inherit pkgs;};
-in {
+{pkgs, lib, ...}:
+{
   services.xserver = {
     enable = true;
     displayManager = {
       sddm.enable = lib.mkDefault true;
-      sddm.theme = "${sddmTheme}";
+      sddm.theme = "catppuccin-mocha";
     };
   };
 
   environment.systemPackages = with pkgs; [
+    (catppuccin-sddm.override {
+      flavor = "mocha";
+      loginBackground = true;
+      background = "${../../../assets/wallpapers/login.png}";
+    })
     libsForQt5.qt5.qtquickcontrols2
     libsForQt5.qt5.qtgraphicaleffects
   ];
