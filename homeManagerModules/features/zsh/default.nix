eb774ec7ebfd337e163969141131bb89b630a647
Alkim Ake Gozen <alkimake@pm.me>
2024-06-14 16:18:24 +0900

    zsh: vim color scheme is catppuccin mocha

--- homeManagerModules/features/zsh/default.nix
+++ homeManagerModules/features/zsh/default.nix
@@ -71,7 +71,7 @@ in {
     export LESS_TERMCAP_so=$'\e[01;33m'
     export LESS_TERMCAP_ue=$'\e[0m'
     export LESS_TERMCAP_us=$'\e[1;4;31m'
-    export vimcolorscheme="gruvbox"
+    export vimcolorscheme="catppuccin-mocha"
 
     # direnv
     export DIRENV_LOG_FORMAT=""
