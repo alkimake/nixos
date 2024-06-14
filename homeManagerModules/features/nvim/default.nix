989193641a72866c6c5b68327c0bbe473d9476da
Alkim Ake Gozen <alkimake@pm.me>
2024-06-14 15:57:14 +0900

    nvim: neotree integrations is done

--- homeManagerModules/features/nvim/default.nix
+++ homeManagerModules/features/nvim/default.nix
@@ -1,7 +1,9 @@
 {pkgs, inputs, ...}: {
    imports = [
     inputs.nixvim.homeManagerModules.nixvim
+    ./keymaps.nix
     ./options.nix
+    ./plugins
   ];
 
  programs.nixvim = {
@@ -20,7 +22,7 @@
         cmp = true;
         gitsigns = true;
         notify = false;
-        nvimtree = true;
+        neotree = true;
         treesitter = true;
       };
     };
@@ -28,6 +30,55 @@
     extraPackages = with pkgs; [
       wl-clipboard
     ];
+    autoCmd =
+      let
+        refresh = ''
+          function()
+            local manager_avail, manager = pcall(require, "neo-tree.sources.manager")
+            if manager_avail then
+              for _, source in ipairs { "filesystem", "git_status", "document_symbols" } do
+                local module = "neo-tree.sources." .. source
+                if package.loaded[module] then manager.refresh(require(module).name) end
+              end
+            end
+          end
+        '';
+      in
+      [
+        # https://github.com/AstroNvim/AstroNvim/blob/v4.7.7/lua/astronvim/plugins/neo-tree.lua#L21-L37
+        {
+          desc = "Open explorer on startup with directory";
+          event = "BufEnter";
+          group = "neotree";
+
+          callback.__raw = ''
+            function()
+              if package.loaded["neo-tree"] then
+                return true
+              else
+                local stats = vim.loop.fs_stat(vim.api.nvim_buf_get_name(0))
+                if stats and stats.type == "directory" then
+                  return true
+                end
+              end
+            end
+          '';
+        }
+        # https://github.com/AstroNvim/AstroNvim/blob/v4.7.7/lua/astronvim/plugins/neo-tree.lua#L25-L35
+        {
+          desc = "Refresh explorer sources when closing lazygit";
+          event = "TermClose";
+          group = "neotree";
+          pattern = "*lazygit*";
+          callback.__raw = refresh;
+        }
+        {
+          desc = "Refresh explorer sources on focus";
+          event = "FocusGained";
+          group = "neotree";
+          callback.__raw = refresh;
+        }
+      ];
   };
 }
 
