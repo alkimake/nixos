3fc0d660e44432aad0c41b231a776734d4d36d8d
Alkim Ake Gozen <alkimake@pm.me>
2024-07-27 22:45:34 +0900

    sddm: catppuccin theme is set

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 - [x] Remove Prism (i don't like it)
 - [ ] Add Catppuccin theme to bat
 - [ ] go back to catppuccin
-- [ ] catppuccin sddm theme is not working for some reason
+- [x] catppuccin sddm theme is not working for some reason
 - [x] nixcolors catppuccin
 - [ ] hyprland scale with default option `auto` and fractional scale
--- nixosModules/features/sddm/default.nix
+++ nixosModules/features/sddm/default.nix
@@ -1,14 +1,28 @@
-{pkgs, lib, ...}:
 {
+  pkgs,
+  lib,
+  ...
+}: {
   services.xserver = {
     enable = true;
-    displayManager = {
-      sddm.enable = lib.mkDefault true;
-    };
+  };
+
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "catppuccin-mocha";
+    package = pkgs.kdePackages.sddm;
   };
 
   environment.systemPackages = with pkgs; [
     libsForQt5.qt5.qtquickcontrols2
     libsForQt5.qt5.qtgraphicaleffects
+    (catppuccin-sddm.override
+      {
+        flavor = "mocha";
+        font = "Noto Sans";
+        fontSize = "9";
+        background = "${../../../assets/wallpapers/login.png}";
+        loginBackground = true;
+      })
   ];
 }
