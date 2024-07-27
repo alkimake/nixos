0da67be2e4af9325c71bb454fbbac1f1212ea38f
Alkim Ake Gozen <alkimake@pm.me>
2024-07-27 22:51:53 +0900

    nix: enable automatic gc

--- hosts/X1/configuration.nix
+++ hosts/X1/configuration.nix
@@ -28,6 +28,12 @@
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
