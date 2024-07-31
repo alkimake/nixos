2ee152a424c80123a0caabf99e40f8a66855a34a
Alkim Ake Gozen <alkimake@pm.me>
2024-07-31 18:57:15 +0900

    hyprland: master is new setting removed

--- homeManagerModules/features/hyprland/default.nix
+++ homeManagerModules/features/hyprland/default.nix
@@ -168,10 +168,7 @@ in {
         };
 
         master = {
-          # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
-          new_is_master = true;
-          # soon :)
-          # orientation = "center";
+          orientation = "center";
         };
 
         gestures = {
